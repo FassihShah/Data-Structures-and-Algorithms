117525422eeb1646c39678474fad191f6c3b1d6d
Fassih Shah <139495501+FassihShah@users.noreply.github.com>
2024-08-04 16:04:17 +0500

    Add files via upload

---
+++ ADT's/Hash Table/Hashmap.h
@@ -0,0 +1,27 @@
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+using namespace std;
+template<typename T>
+class Pair
+{
+public:
+    T val;
+    int key;
+    Pair<T>* next;
+    Pair(T x, int y) :val(x), key(y), next(nullptr) {}
+};
+template<typename T>
+class HashMap
+{
+    Pair<T>** list;
+    int Hashing(int key);
+public:
+    HashMap();
+    void put(int key, T value);
+    T get(int key);
+    void remove(int key);
+    bool contains(int key);
+};
+template class Pair<int>;
+template class HashMap<int>;
+#endif
